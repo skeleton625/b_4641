284b9bfa168232c1dbc6e001698cbd6c6c5c0a96
WaniFi <skelpd24@gmail.com>
2018-08-23 21:30:12 +0900

    4641 문제 풀이 개선 중

--- baekjoon_4641/main.cpp
+++ baekjoon_4641/main.cpp
@@ -1,24 +1,22 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+#include <cstdio>
 
-int n[15] = { 1 }, i, c;
+int n[15], c, i;
 int main() {
 	while (1) {
-		scanf_s("%d", &n[0]);
-		if (n[0] == -1) break;
-		for (i = 1; n[i-1] != 0; i++) scanf_s("%d", &n[i]);
-		sort(n, n+i);
-		for (int j = 1; j <= i; j++) {
-			for (int k = i; k > j; k--) { 
-				if( n[j]*2 == n[k]) {
+		while (scanf_s("%d", &n[i++])) {
+			if (n[0] == -1) return 0;
+			else if (!n[i - 1]) break;
+		}
+		for (int j = 0; j < i-1; j++) {
+			for (int k = 0; k < i-1; k++) {
+				if (n[j] * 2 == n[k]) {
 					c++;
 					break;
 				}
 			}
 		}
-		cout << c << endl;
-		c = 0;
+		printf("%d\n", c);
+		c = 0; i = 0;
 	}
 	return 0;
 }
